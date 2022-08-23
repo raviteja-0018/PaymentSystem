04b24986abb401e6909f51684a66c7e69964d622
raviteja-0018 <domakonda_raviteja18@outlook.com>
2022-08-23 12:11:41 +0530

    Project created

---
+++ paymentsystem/src/test/java/com/payments/paymentsystem/PaymentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payments.paymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
